9647ac4f52487a419470f44dfb477766bb02bca8
iskandarsuhaimi <iskandarsuhaimi@ASUS.local>
2019-04-19 23:59:35 +0700

    make route

--- app/controllers/chat_controller_controller.rb
+++ app/controllers/chat_controller_controller.rb
@@ -1,11 +1,29 @@
+require 'unirest'
+
 class ChatControllerController < ApplicationController
-    access_token = ""
-    apiurl = "https://qisme.qiscus.com/api/v1/chat/conversations/"
+    access_token = ''
+    apiurl = 'https://qisme.qiscus.com/api/v1/chat/conversations/'
     headers = {
-        "Content-Type" => "application/json",
-        "Content-Type" => "multipart/form-data"
+        'Content-Type' => 'application/json',
+        'Content-Type' => 'multipart/form-data'
     }
     attr_accessor :qismeResponse
 
+    private
+    def getResponse
+        if request.headers['Content-Type'] == 'application/json'
+            self.qismeResponse = JSON.parse(request.body.read)
+        else
+            # application/x-www-form-urlencoded
+            self.qismeResponse = params.as_json
+        end
+        File.open('log-comment.txt','w') do |f|
+            f.write(self.qismeResponse)
+        end    
+    end
     
+    public
+    def run
+        self.getResponse
+    end    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  constraints subdomain: "hooks" do
+    post '/' => 'chat_controller#run', as: :receive_webhooks
+  end
 end
