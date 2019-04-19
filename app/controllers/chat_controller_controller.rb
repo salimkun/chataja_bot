117fa8cc085f5a1a0e618220f5d9704a8c90f795
iskandarsuhaimi <iskandarsuhaimi@ASUS.local>
2019-04-19 23:30:22 +0700

    make controller

--- app/controllers/chat_controller_controller.rb
+++ app/controllers/chat_controller_controller.rb
@@ -1,2 +1,11 @@
 class ChatControllerController < ApplicationController
+    access_token = ""
+    apiurl = "https://qisme.qiscus.com/api/v1/chat/conversations/"
+    headers = {
+        "Content-Type" => "application/json",
+        "Content-Type" => "multipart/form-data"
+    }
+    attr_accessor :qismeResponse
+
+    
 end
