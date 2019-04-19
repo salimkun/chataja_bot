aec4adda394e3f99d28f535de5fcd5cddeb73be6
iskandarsuhaimi <iskandarsuhaimi@ASUS.local>
2019-04-20 00:53:30 +0700

    make controller

--- app/controllers/chat_controller_controller.rb
+++ app/controllers/chat_controller_controller.rb
@@ -5,9 +5,9 @@ class ChatControllerController < ApplicationController
 
     access_token = 'eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjo0Njk1NSwidGltZXN0YW1wIjoiMjAxOS0wNC0xOSAxNzowOTo0NiArMDAwMCJ9.J4z039nhHfvnRbzwPDebrPTf3nAsqX7GdpMZvgq9zUo'
     apiurl = 'https://qisme.qiscus.com/api/v1/chat/conversations/'
-    headers = {
+    headers = headers:{ 
         'Content-Type' => 'application/json',
-        'Content-Type' => 'multipart/form-data'
+        'Content-Type' => 'multipart/form-data' 
     }
     attr_accessor :qismeResponse
 
@@ -22,6 +22,37 @@ class ChatControllerController < ApplicationController
             f.write(JSON.pretty_generate(self.qismeResponse))
         end    
     end
+
+    def replyCommandButton(display_name,room_id)
+        comment = 'Halo, '+display_name+' ini adalah contoh payload button yang bisa kamu coba'
+        payload = {
+            'text' => comment,
+            'buttons' => [
+                {
+                    "label" => "Tombol Reply Text",
+                    "type" => "postback",
+                    "payload" => [
+                        "url" => "#",
+                        "method" => "get",
+                        "payload" => "null"
+                    ]
+                },
+                {
+                    "label" => "Tombol Link",
+                    "type" => "link",
+                    "payload" => [
+                        "url" => "https://www.google.com",
+                    ]
+                }
+            ]
+        }
+        replay = parameters:{ 
+            'access_token' => self.access_token
+            'topic_id' => room_id
+            'payload' => payload 
+        }
+        post_comment = Unirest.post self.apiurl, self.headers, replay
+    end    
     
     def run
         self.getResponse
