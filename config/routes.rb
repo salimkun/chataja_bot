552b165c547832ba337c21fc797eba407f33fd23
iskandarsuhaimi <iskandarsuhaimi@ASUS.local>
2019-04-20 13:03:05 +0700

    make route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
+# @authors: Iskandar Suhaimi
+# @email: hello@iskandarsuhaimi.com 
+
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   post '/' => 'chat_controller#run', as: "receive_webhooks"
 end
