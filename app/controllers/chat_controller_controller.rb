db937630c4ef2fff07e0001f22a931250b367599
iskandarsuhaimi <iskandarsuhaimi@ASUS.local>
2019-04-20 12:38:05 +0700

    make controller

--- app/controllers/chat_controller_controller.rb
+++ app/controllers/chat_controller_controller.rb
@@ -1,9 +1,10 @@
 require 'unirest'
 
 class ChatControllerController < ApplicationController
-    skip_before_action :verify_authenticity_token 
-    attr_accessor :qismeResponse, :access_token, :apiurl, :headers
+    skip_before_action :verify_authenticity_token #skip verify token rails
+    attr_accessor :qismeResponse, :access_token, :apiurl, :headers #set-get atribut controller
 
+    #inisiasi nilai atribut
     def initialize()
         @access_token = 'eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjo0Njk1NSwidGltZXN0YW1wIjoiMjAxOS0wNC0wNSAwNjoxMjo0MSArMDAwMCJ9.u5PHjfNPrRL_nhh5S-UUSNLBr2kKBlBI89px2L2jjdg'
         @apiurl = 'https://qisme.qiscus.com/api/v1/chat/conversations/'
@@ -12,18 +13,22 @@ class ChatControllerController < ApplicationController
         }
     end    
 
+    #ambil dan tampung response data dari webhook
     def getResponse
         if request.headers['Content-Type'] == 'application/json'
             self.qismeResponse = JSON.parse(request.body.read)
         else
-            # application/x-www-form-urlencoded
+            #application/x-www-form-urlencoded
             self.qismeResponse = params.as_json
         end
+
+        #siapkan log untuk memastikan data terambil
         File.open('log-comment.txt','w') do |f|
             f.write(JSON.pretty_generate(self.qismeResponse))
         end    
     end
 
+    #contoh penggunaan api post-comment untuk jenis button
     def replyCommandButton(display_name, room_id)
         comment = 'Halo, '+display_name+' ini adalah contoh payload button yang bisa kamu coba'
         payload = {
@@ -57,6 +62,7 @@ class ChatControllerController < ApplicationController
         render :json => post_comment.raw_body
     end
     
+    #contoh penggunaan api post-comment untuk jenis text
     def replyCommandText(display_name, message_type, room_id)
         comment = 
         'Maaf '+display_name+', command yang kamu ketik salah. Jenis pesan kamu adalah '+message_type+'. Silahkan coba command berikut :\n'+
@@ -207,18 +213,43 @@ class ChatControllerController < ApplicationController
             'payload' => payload.to_json
         }
         card  = Unirest.post(self.apiurl+'post_comment', headers: self.headers, parameters: replay);
-        card.raw_body
+        render :json => card.raw_body
     end
 
+    #fungsi untuk jalankan bot
     def run
-        self.getResponse
+        self.getResponse 
         chat = Chat.new(
             self.qismeResponse['chat_room']['qiscus_room_id'],
             self.qismeResponse['message']['text'],
             self.qismeResponse['message']['type'],
             self.qismeResponse['from']['fullname']
         )
-        #self.replyCommandButton(chat.sender, chat.room_id)
-        self.replyCommandText(chat.sender, chat.message_type, chat.room_id)
+
+        #cek pesan dari chat tidak kosong
+        if chat.message != nil
+            #cari chat yang mengandung '/' untuk menjalankan command bot
+            find_slash = chat.message.scan('/')
+            if find_slash[0] == '/'
+                #ambil nilai text setelah karakter '/'
+                command = chat.message.split('/')
+                if command[1] != nil
+                    case command[1]
+                        when 'location'
+                            self.replyCommandLocation(chat.room_id)
+                        when 'carousel':
+                            self.replyCommandCarousel(chat.room_id)
+                        when 'button':
+                            self.replyCommandButton(chat.sender, chat.room_id)
+                        when 'card':
+                            self.replyCommandCard(chat.room_id)
+                        else
+                            self.replyCommandText(chat.sender, chat.message_type, chat.room_id)            
+                    end
+                else
+                    self.replyCommandText(chat.sender, chat.message_type, chat.room_id)
+                end
+            end
+        end
     end    
 end
