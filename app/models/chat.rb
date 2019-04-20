38363f9d49f7606b19ea0a09e2b4f99b94fcfd77
iskandarsuhaimi <iskandarsuhaimi@ASUS.local>
2019-04-20 13:01:19 +0700

    make model

--- app/models/chat.rb
+++ app/models/chat.rb
@@ -1,6 +1,6 @@
 class Chat 
-    include ActiveModel::Model
-    attr_reader :room_id, :message, :message_type, :sender
+    include ActiveModel::Model  #set agar model di rails bisa jalan sesuai kebutuhan
+    attr_reader :room_id, :message, :message_type, :sender #set atribut model untuk menampung response data chat
 
     def initialize(room_id, message, message_type, sender)
         @room_id = room_id
